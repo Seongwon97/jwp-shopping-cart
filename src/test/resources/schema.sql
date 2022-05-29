ca41ad5faf115fc3c677530dd57bf5fb7fc45393
Seongwon97 <osw970325@naver.com>
2022-05-29 22:02:36 +0900

    refactor(schema): ddl 수정
    - 현재 불필요한 테이블 제거
    - ddl 재정의

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,74 +1,8 @@
-drop table if exists orders_detail;
-
-drop table if exists orders;
-
-drop table if exists cart_item;
-
-drop table if exists product;
-
-drop table if exists member;
-
-create table member
+create table if not exists member
 (
-    id       bigint       not null auto_increment,
-    email varchar(255) not null,
+    id bigint auto_increment not null,
+    email varchar(255) not null unique,
     name varchar(10) not null,
     password varchar(255) not null,
-    primary key (id)
-) engine=InnoDB default charset=utf8mb4;
-
-alter table member
-    add unique key (email);
-
-create table product
-(
-    id        bigint       not null auto_increment,
-    name      varchar(255) not null,
-    price     integer      not null,
-    image_url varchar(255),
-    primary key (id)
-) engine=InnoDB default charset=utf8mb4;
-
-create table cart_item
-(
-    id          bigint not null auto_increment,
-    member_id bigint not null,
-    product_id  bigint not null,
-    primary key (id)
-) engine=InnoDB default charset=utf8mb4;
-
-alter table cart_item
-    add constraint fk_cart_item_to_member
-        foreign key (member_id) references member (id);
-
-alter table cart_item
-    add constraint fk_cart_item_to_product
-        foreign key (product_id) references product (id);
-
-create table orders
-(
-    id          bigint not null auto_increment,
-    member_id bigint not null,
-    primary key (id)
-) engine=InnoDB default charset=utf8mb4;
-
-alter table orders
-    add constraint fk_orders_to_member
-        foreign key (member_id) references member (id);
-
-create table orders_detail
-(
-    id         bigint  not null auto_increment,
-    orders_id  bigint  not null,
-    product_id bigint  not null,
-    quantity   integer not null,
-    primary key (id)
-) engine=InnoDB default charset=utf8mb4;
-
-alter table orders_detail
-    add constraint fk_orders_detail_to_orders
-        foreign key (orders_id) references orders (id);
-
-alter table orders_detail
-    add constraint fk_orders_detail_to_product
-        foreign key (product_id) references product (id);
+    primary key(id)
+);
